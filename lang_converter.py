538ab40ee825bdcc989c6a3ba12a63530fc3323d
abideen305 <59992054+abideen305@users.noreply.github.com>
2022-04-09 03:46:02 +0100

    Update lang_converter.py

--- lang_converter.py
+++ lang_converter.py
@@ -1,55 +1,27 @@
 import streamlit as st
 
 st.title('Language Converter by Abideen')
+st.sidebar.write('Welcome to my page')
+st.sidebar.button('click me')
 
-@st.cache
-def lang_conv():
-    from textblob import TextBlob
+from googletrans import Translator, constants
+from pprint import pprint
 
-    text = input("Enter a text to translate: ")
-    blob = TextBlob(text)
+text = st.text_input('Enter a text to translate: ')
 
-    print(f'''
+# init the Google API translator
+translator = Translator()
 
-    Your text "{text}" in Arabic is : "{blob.translate(from_lang='auto', to='ar')}"
-    Your text "{text}" in China is : "{blob.translate(from_lang='auto', to='zh')}"
-    Your text "{text}" in French is : '{blob.translate(from_lang='auto', to='fr')}'
-    Your text "{text}" in Hindi is : '{blob.translate(from_lang='auto', to='hi')}'
-
-    ''')
-    return blob.translate(from_lang='auto', to='ar')
-
-    que = input('Do you need more info about the word you converted?').lower()
-
-    if que == 'y' or que == 'yes':
-        count_w = blob.word_counts
-        print(f'''This is your word counts:
-
-                    {count_w}  
-                ''')
-        corr = blob.correct()
-        print(f'''
-
-            {blob.words}
-
-        Polarity: {blob.sentiment.polarity}
-
-        Subjectivity: {blob.sentiment.subjectivity}
-
-        {blob.sentences}
+# translate a spanish text to english text (by default)
+translation = translator.translate(text, dest='ar')
+tr_ar = translation.text
+translation = translator.translate(text, dest='fr')
+tr_fr = translation.text
+translation = translator.translate(text, dest='hi')
+tr_hi = translation.text
 
 
-        ''')
-        if corr != text:
-            print('I found a spelling issue in your text and I corrected it')
-            print(f'''This is the correct text
-
-                    {corr}
-
-                ''')
-    else:
-        print("Thank you")
-
-lang_conv()
-st.write('Welcome')
+st.write(f"Your text: {text} in arabic is: {tr_ar}")
+st.write(f"Your text: {text} in  french is: {tr_fr}")
+st.write(f"Your text: {text} in hindi is: {tr_hi}")
 
